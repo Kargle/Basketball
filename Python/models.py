162fb079bfeb5bbd97dd95c7ca8b264ed4da2d5a
Real-Karisch <jack.karisch@gmail.com>
2020-04-20 23:14:04 -0500

    Incorporated ATK's changes; checked a bunch of models

--- Python/models.py
+++ Python/models.py
@@ -55,4 +55,106 @@ def betterRecordWins(teamA, teamB, refDF):
     else:
         return teamB
 
+def betterPtsDifWins(teamA, teamB, refDF):
+    teamADF = refDF[refDF['LTeamID'] == teamA]
+    if teamADF.empty:
+        teamADF = refDF[refDF['WTeamID'] == teamA]
+        teamAPtsDif = float(teamADF.loc[teamADF.index[0], 'WPtsDif'])
+    else:
+        teamAPtsDif = float(teamADF.loc[teamADF.index[0], 'LPtsDif'])
 
+    teamBDF = refDF[refDF['LTeamID'] == teamB]
+    if teamBDF.empty:
+        teamBDF = refDF[refDF['WTeamID'] == teamB]
+        teamBPtsDif = float(teamBDF.loc[teamBDF.index[0], 'WPtsDif'])
+    else:
+        teamBPtsDif = float(teamBDF.loc[teamBDF.index[0], 'LPtsDif'])
+
+    if teamAPtsDif == teamBPtsDif:
+        rand = np.random.random()
+        if rand < 0.5:
+            return teamA
+        else:
+            return teamB
+    elif teamAPtsDif > teamBPtsDif:
+        return teamA
+    else:
+        return teamB
+
+def betterPtsDifPGWins(teamA, teamB, refDF):
+    teamADF = refDF[refDF['LTeamID'] == teamA]
+    if teamADF.empty:
+        teamADF = refDF[refDF['WTeamID'] == teamA]
+        teamAPtsPGDif = float(teamADF.loc[teamADF.index[0], 'WPtsPGDif'])
+    else:
+        teamAPtsPGDif = float(teamADF.loc[teamADF.index[0], 'LPtsPGDif'])
+
+    teamBDF = refDF[refDF['LTeamID'] == teamB]
+    if teamBDF.empty:
+        teamBDF = refDF[refDF['WTeamID'] == teamB]
+        teamBPtsPGDif = float(teamBDF.loc[teamBDF.index[0], 'WPtsPGDif'])
+    else:
+        teamBPtsPGDif = float(teamBDF.loc[teamBDF.index[0], 'LPtsPGDif'])
+
+    if teamAPtsPGDif == teamBPtsPGDif:
+        rand = np.random.random()
+        if rand < 0.5:
+            return teamA
+        else:
+            return teamB
+    elif teamAPtsPGDif > teamBPtsPGDif:
+        return teamA
+    else:
+        return teamB
+
+def betterFTPercWins(teamA, teamB, refDF):
+    teamADF = refDF[refDF['LTeamID'] == teamA]
+    if teamADF.empty:
+        teamADF = refDF[refDF['WTeamID'] == teamA]
+        teamAFTPerc = float(teamADF.loc[teamADF.index[0], 'WFTPerc'])
+    else:
+        teamAFTPerc = float(teamADF.loc[teamADF.index[0], 'LFTPerc'])
+
+    teamBDF = refDF[refDF['LTeamID'] == teamB]
+    if teamBDF.empty:
+        teamBDF = refDF[refDF['WTeamID'] == teamB]
+        teamBFTPerc = float(teamBDF.loc[teamBDF.index[0], 'WFTPerc'])
+    else:
+        teamBFTPerc = float(teamBDF.loc[teamBDF.index[0], 'LFTPerc'])
+
+    if teamAFTPerc == teamBFTPerc:
+        rand = np.random.random()
+        if rand < 0.5:
+            return teamA
+        else:
+            return teamB
+    elif teamAFTPerc > teamBFTPerc:
+        return teamA
+    else:
+        return teamB
+
+def betterAstTORatioWins(teamA, teamB, refDF):
+    teamADF = refDF[refDF['LTeamID'] == teamA]
+    if teamADF.empty:
+        teamADF = refDF[refDF['WTeamID'] == teamA]
+        teamAATR = float(teamADF.loc[teamADF.index[0], 'WATR'])
+    else:
+        teamAATR = float(teamADF.loc[teamADF.index[0], 'LATR'])
+
+    teamBDF = refDF[refDF['LTeamID'] == teamB]
+    if teamBDF.empty:
+        teamBDF = refDF[refDF['WTeamID'] == teamB]
+        teamBATR = float(teamBDF.loc[teamBDF.index[0], 'WATR'])
+    else:
+        teamBATR = float(teamBDF.loc[teamBDF.index[0], 'LATR'])
+
+    if teamAATR == teamBATR:
+        rand = np.random.random()
+        if rand < 0.5:
+            return teamA
+        else:
+            return teamB
+    elif teamAATR > teamBATR:
+        return teamA
+    else:
+        return teamB
